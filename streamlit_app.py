2b55a99f05a957a68420429edb0c30be52e10c7a
lmcforlab <135080275+lmcforlab@users.noreply.github.com>
2023-05-31 10:50:44 +0800

    Update streamlit_app.py
    corrected the syntax error found

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc((fruits_selected)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 # streamlit.dataframe(my_fruit_list)
