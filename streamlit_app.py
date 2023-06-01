2fdba4c7b0cf961cf8f02b4903d510b716493e9b
lmcforlab <135080275+lmcforlab@users.noreply.github.com>
2023-06-01 15:03:40 +0800

    Update streamlit_app.py
    Added Try... If.. Else... Except

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,20 +31,30 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 # New section to display fruityvice api response
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+    
 streamlit.write('The user entered ', fruit_choice)
 
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response)
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # normalize the fruity vice data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # shows the normalized data to the screen
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
