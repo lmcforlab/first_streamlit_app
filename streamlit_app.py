367a475f4507b09d85d0c3f983bd90bbc478401c
lmcforlab <135080275+lmcforlab@users.noreply.github.com>
2023-06-01 08:44:35 +0800

    Update streamlit_app.py
    normalize the fruity vice data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,4 +29,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+
+# normalize the fruity vice data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# shows the normalized data to the screen
+streamlit.dataframe(fruityvice_normalized)
